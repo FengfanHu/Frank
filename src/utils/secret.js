0d2bd9b980e015f4b3b1249dc910ad3978f27f1e
FrankHu <471048894@qq.com>
2021-02-28 22:58:52 +0800

    remove secret

---
+++ src/utils/secret.js
@@ -0,0 +1,9 @@
+export const QiniuConfig = {
+  domain: 'qothaidfx.hd-bkt.clouddn.com',
+  bucket: 'blogoffrank',
+  AK: '8LP_Ntoig_3ozdtBVtewWHGOsN6Z0uC6Zu7DGWXo',
+  SK: 'Yvd4uZhctRSptuMCom8XNQLAbqFac8VVjjwDiCNL'
+}
+
+
+
